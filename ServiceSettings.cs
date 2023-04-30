c4d070c61e557487e6345b484ea57b7c24509051
anupamsrepos <103147228+anupamsrepos@users.noreply.github.com>
2023-04-30 08:42:04 -0700

    Moved common codes to Play.Common class lib project and created nuget package (#1)

--- ServiceSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace Play.Catalog.Service.Settings
-{
-    //This is to map with the app config
-    public class ServiceSettings
-    {
-        public string ServiceName { get; init; }
-    }
-}
